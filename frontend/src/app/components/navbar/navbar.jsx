49c3c94a211568552bf55e2234ae171805cbf453
MuhammadAffan-06 <maffan2202@gmail.com>
2024-11-12 10:56:03 +0500

    navbar buttons activation

--- frontend/src/app/components/navbar/navbar.jsx
+++ frontend/src/app/components/navbar/navbar.jsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
 import "@/app/components/navbar/navbar.css";
 export default function Navbar() {
+  const navRoutes = () => {
+    window.location.href = "https://mind-sync-u9h4.vercel.app/auth";
+  };
   return (
     <>
       <div className="navbar-parent">
@@ -12,12 +16,12 @@ export default function Navbar() {
           height={130}
         />
         <div className="navbar-buttons">
-          <button>Login</button>
-          <button className="colorButton">Sign Up</button>
+          <button onClick={() => navRoutes()}>Login</button>
+          <button className="colorButton" onClick={() => navRoutes()}>
+            Sign Up
+          </button>
         </div>
       </div>
-
-
     </>
   );
 }
