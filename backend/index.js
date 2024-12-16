c2eff3e13285f36e7a413f8deb8c7abeb7d4ae64
MuhammadAffan-06 <maffan2202@gmail.com>
2024-12-17 03:43:12 +0500

    urls changed to development

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // Update to your frontend URL
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
