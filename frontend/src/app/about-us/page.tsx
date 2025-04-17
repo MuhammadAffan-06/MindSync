fa8a56ae07c72efe452676265fb1ffcc15a36356
MuhammadAffan-06 <maffan2202@gmail.com>
2025-04-17 16:21:41 +0500

    minor fixes

--- frontend/src/app/about-us/page.tsx
+++ frontend/src/app/about-us/page.tsx
@@ -1,8 +1,10 @@
-'use client';
+"use client";
 
+// import Nav from "@/app/components/nav/nav";
 import Nav from "@/app/components/nav/nav";
+// import Sidebar from "@/app/components/sideBar/sideBar";
 import Sidebar from "@/app/components/sideBar/sideBar";
-import "@/app/about-us/about-us.css";
+import "./about-us.css";
 import Link from "next/link";
 
 export default function About() {
@@ -14,14 +16,16 @@ export default function About() {
         <div className="content-container">
           <h1 className="about-title">About Us</h1>
           <p className="about-paragraph">
-            At MindSync, we are passionate about transforming presentations into meaningful interactions. 
-            Our platform empowers educators, trainers, and professionals to create dynamic and engaging 
-            sessions that foster real-time collaboration, smart assessments, and impactful learning experiences. 
-            Whether it’s in classrooms, boardrooms, or online events, MindSync bridges the gap between presenters 
-            and participants, making every session memorable and effective.
+            At MindSync, we are passionate about transforming presentations into
+            meaningful interactions. Our platform empowers educators, trainers,
+            and professionals to create dynamic and engaging sessions that
+            foster real-time collaboration, smart assessments, and impactful
+            learning experiences. Whether it’s in classrooms, boardrooms, or
+            online events, MindSync bridges the gap between presenters and
+            participants, making every session memorable and effective.
           </p>
           <Link href="/dashboard">
-          <button className="back-button">Back to Dashboard</button>
+            <button className="back-button">Back to Dashboard</button>
           </Link>
         </div>
       </div>
--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,50 +1,60 @@
 "use client";
 
+// import Nav from "@/app/components/nav/nav";
 import Nav from "@/app/components/nav/nav";
 
-import "@/app/admin/admin.css";
+// import "@/app/admin/admin.css";
+import "./admin.css";
 import Image from "next/image";
-
 
 export default function Admin() {
   return (
     <>
       <Nav />
-     <div className="u-r-managment">
+      <div className="u-r-managment">
         <h1>User & Role Managment</h1>
         <div className="wrapper-u-r-m">
-            <div className="box">
-                <Image src="user-profile-avatar2.svg" alt="Image" className="icon-main" />
-                <div className="boxer">
-                <div className="tech">Teacher</div> 
-                <div className="dropdown">
-                    <Image src="dropdown2.svg" alt="Image" />
-                    </div>
-                </div>
-                
-                </div>
-            <div className="box">
-                <Image src="user-profile-avatar2.svg" alt="Image" className="icon-main" />
-                <div className="boxer">
-                <div className="tech">Students</div> 
-                <div className="dropdown">
-                    <Image src="dropdown2.svg" alt="Image" />
-                    </div>
-                </div>
-                
-                </div>
-            <div className="box">
-                <Image src="user-profile-avatar2.svg" alt="Image" className="icon-main" />
-                <div className="boxer">
-                <div className="tech">Students</div> 
-                <div className="dropdown">
-                    <Image src="dropdown2.svg" alt="Image" />
-                    </div>
-                </div>
-                
-                </div>
+          <div className="box">
+            <Image
+              src="user-profile-avatar2.svg"
+              alt="Image"
+              className="icon-main"
+            />
+            <div className="boxer">
+              <div className="tech">Teacher</div>
+              <div className="dropdown">
+                <Image src="dropdown2.svg" alt="Image" />
+              </div>
+            </div>
+          </div>
+          <div className="box">
+            <Image
+              src="user-profile-avatar2.svg"
+              alt="Image"
+              className="icon-main"
+            />
+            <div className="boxer">
+              <div className="tech">Students</div>
+              <div className="dropdown">
+                <Image src="dropdown2.svg" alt="Image" />
+              </div>
+            </div>
+          </div>
+          <div className="box">
+            <Image
+              src="user-profile-avatar2.svg"
+              alt="Image"
+              className="icon-main"
+            />
+            <div className="boxer">
+              <div className="tech">Students</div>
+              <div className="dropdown">
+                <Image src="dropdown2.svg" alt="Image" />
+              </div>
+            </div>
+          </div>
         </div>
-     </div>
+      </div>
     </>
   );
 }
