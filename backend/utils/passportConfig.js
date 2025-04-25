6e7f1dc09ac8ddca35468c1848a2e205488e10c9
MuhammadAffan-06 <maffan2202@gmail.com>
2025-04-26 04:30:18 +0500

    moving to prod

--- backend/utils/passportConfig.js
+++ backend/utils/passportConfig.js
@@ -8,8 +8,9 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-       callbackURL: `${process.env.SERVER_DOMAIN}:${process.env.PORT}/auth/google/callback`,
+      //  callbackURL: `${process.env.SERVER_DOMAIN}:${process.env.PORT}/auth/google/callback`,
       //callbackURL: "https://mindsync-hpauf7bfegd9dudz.westindia-01.azurewebsites.net/auth/google/callback",
+      callbackURL: "https://mindsync-cbbee2f6dmf9hma5.uaenorth-01.azurewebsites.net/auth/google/callback"
       
     },
     async (accessToken, refreshToken, profile, done) => {
--- frontend/src/app/components/utils/api.ts
+++ frontend/src/app/components/utils/api.ts
@@ -41,9 +41,9 @@ export type PathResponseMap = {
   "/presentation/isLive": PresentationIsLiveResponse;
 };
 
-const serverBaseUrl = "http://localhost:5000";
-// const serverBaseUrl = "https://mindsync-hpauf7bfegd9dudz.westindia-01.azurewebsites.net"
-
+// const serverBaseUrl = "http://localhost:5000";
+// const serverBaseUrl = "https://mindsync-hpauf7bfegd9dudz.westindia-01.azurewebsites.net" Salman Development Environment
+const serverBaseUrl = "https://mindsync-cbbee2f6dmf9hma5.uaenorth-01.azurewebsites.net"
 
 function getHeaders(): HeadersInit {
   const headers: Record<string, string> = {
