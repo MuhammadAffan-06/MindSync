a5a2b090539e55eea05c39fafb171909c8f4ba6a
MuhammadAffan-06 <maffan2202@gmail.com>
2025-04-26 06:47:18 +0500

    back to prod

--- backend/controller/auth-controller.js
+++ backend/controller/auth-controller.js
@@ -2,7 +2,7 @@ const User = require("../models/users");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const validator = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 // Sign-up API
 const signup = async (req, res) => {
